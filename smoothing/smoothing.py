d8b70b5657eb72c2e943a7f59878f3c1b1618d48
cedricmontero <cedric.montero@esrf.fr>
2013-03-13 11:45:35 +0100

    Deleting comments in smoothing.py

--- smoothing/smoothing.py
+++ smoothing/smoothing.py
@@ -71,28 +71,6 @@ def SmoothInterp(x0,y0,xi,wMin,nMin):
 	return numpy.array(yi),numpy.array(dyi),numpy.array(ddyi)
 
 
-
-"""
-def smooth(x,window_len=11,window='hanning'):
-   ##The signal is prepared by introducing reflected window-length copies of the signal at both ends so that boundary effect are minimized in the beginning and end part of the output signal.
-   if x.ndim != 1:
-      raise ValueError, "smooth only accepts 1 dimension arrays."
-   if x.size < window_len:
-      raise ValueError, "Input vector needs to be bigger than window size."
-   if window_len<3:
-      return x
-   if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
-      raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
-   s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
-   if window == 'flat': #moving average
-      w=np.ones(window_len,'d')
-   else:
-      w=eval('np.'+window+'(window_len)')
-   y=np.convolve(w/w.sum(),s,mode='valid')
-   y = y[int(window_len/2)-1:-1-int(window_len/2)+1]
-   return y
-"""
-
 def LinInterp(x0,y0,xi):
 	"Linear interpolation of (x0,y0) data at xi values"
 	if xi[0]<x0[0] or xi[-1]>x0[-1]:
